687a4e07b07181c2f2ec846f66d20a94db7b888e
Itsuyo Tsukimori <112790054+Abyss-Seeker@users.noreply.github.com>
2026-01-30 18:56:48 +0800

    Import index.css for styling

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // 👈 添加这行
 import App from './App';
 
 const rootElement = document.getElementById('root');
